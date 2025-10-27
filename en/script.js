45ad29836d796f453420ed18b8bbcfb28fec2707
Snowy <qbqtcx@gmail.com>
2025-10-27 22:25:14 +0100

    Create script.js

---
+++ en/script.js
@@ -0,0 +1,19 @@
+const titles = [
+    "Snowy",
+    "sNowy",
+    "snOwy",
+    "snoWy",
+    "snowY",
+    "snoWy",
+    "snOwy",
+    "sNowy"
+];
+
+let index = 0;
+
+function changeTitle() {
+    document.title = titles[index];
+    index = (index + 1) % titles.length;
+}
+
+setInterval(changeTitle, 50);
