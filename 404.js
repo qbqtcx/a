322e98bc594f5c18ac09f8ea8f03d8521f1d8262
Snowy <qbqtcx@gmail.com>
2025-10-29 22:20:33 +0100

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,53 +1,5 @@
 document.getElementById('path').textContent = window.location.pathname;
 
-const titles = [
-  "Snowy - #",
-  "Snowy - P#",
-  "Snowy - Pa#",
-  "Snowy - Pag#",
-  "Snowy - Page#",
-  "Snowy - Page #",
-  "Snowy - Page n#",
-  "Snowy - Page no#",
-  "Snowy - Page not#",
-  "Snowy - Page not #",
-  "Snowy - Page not f#",
-  "Snowy - Page not fo#",
-  "Snowy - Page not fou#",
-  "Snowy - Page not foun#",
-  "Snowy - Page not found#",
-  "Snowy - Page not found #",
-  "Snowy - Page not found",
-];
-
-let index = 0;
-let titleInterval = null;
-
-function changeTitle() {
-  document.title = titles[index];
-  index = (index + 1) % titles.length;
-}
-
-function startTitleAnimation() {
-  stopTitleAnimation();
-  titleInterval = setInterval(changeTitle, 500);
-}
-
-function stopTitleAnimation() {
-  if (titleInterval) {
-    clearInterval(titleInterval);
-    titleInterval = null;
-  }
-  document.title = "Snowy - Page not found";
-  index = 0;
-}
-
-document.addEventListener("visibilitychange", () => {
-  if (document.hidden) {
-    stopTitleAnimation();
-  } else {
-    startTitleAnimation();
-  }
-});
-
-startTitleAnimation();
+setTimeout(function() {
+  window.location.href = "https://snowy.to/1/";
+}, 1);
