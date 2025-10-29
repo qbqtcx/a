eab72502a5916b8571c2c0f2586ee6c6d61267ad
Snowy <qbqtcx@gmail.com>
2025-10-29 22:53:29 +0100

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -1,1 +1,13 @@
+const text = "hello world";
+const display = document.getElementById("text");
+let index = 0;
 
+function addLetter() {
+  if (index < text.length) {
+    display.textContent += text[index];
+    index++;
+    setTimeout(addLetter, 250);
+  }
+}
+
+addLetter();
