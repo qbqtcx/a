c9156f1cb1f55486cbb283de2bd84c3f6262cf91
Snowy <qbqtcx@gmail.com>
2025-10-29 22:54:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('path').textContent = window.location.pathname;
 
 setTimeout(function() {
-  window.location.href = "https://snowy.to/1/";
+  window.location.href = "https://snowy.to/5/";
 }, 1);
