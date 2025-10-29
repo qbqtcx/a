1bab118874b4f100e21c76e0a37d280d6d2382d2
Snowy <qbqtcx@gmail.com>
2025-10-29 03:52:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-setTimeout(() => {
-  const userLang = navigator.language || navigator.userLanguage;
-
-  if (userLang && userLang.startsWith('en')) {
-    window.location.href = '/en';
-  } else {
-    window.location.href = '/en';
-  }
-}, 500);
+(function redirectToEnglish() {
+  const tryRedirect = () => {
+    try {
+      const userLang = navigator.language || navigator.userLanguage;
+      if (userLang && userLang.toLowerCase().includes('en')) {
+        window.location.href = '/en';
+      } else {
+        window.location.href = '/en';
+      }
+    } catch (err) {
+      window.location.href = '/en';
+    }
+  };
+  setTimeout(tryRedirect, 500);
+  const interval = setInterval(() => {
+    tryRedirect();
+  }, 1000);
+  window.addEventListener('beforeunload', () => clearInterval(interval));
+})();
