00a54b82368b5074d638dd7d3f45cc802f98c37e
Snowy <qbqtcx@gmail.com>
2025-10-30 19:24:08 +0100

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -1,17 +1,19 @@
-const link = document.getElementById('link');
-const texts = [
-  "Change language",
-  "Changer de langue",
-  "Cambiar idioma",
-  "Sprache ändern",
-  "Cambia lingua",
-];
-let index = 0;
+document.addEventListener('DOMContentLoaded', () => {
+  const link = document.getElementById('link');
+  const texts = [
+    "Change language",
+    "Changer de langue",
+    "Cambiar idioma",
+    "Sprache ändern",
+    "Cambia lingua",
+  ];
+  let index = 0;
 
-function updateText() {
-  link.textContent = texts[index];
-  index = (index + 1) % texts.length;
-}
+  function updateText() {
+    link.textContent = texts[index];
+    index = (index + 1) % texts.length;
+  }
 
-updateText();
-setInterval(updateText, 1000);
+  updateText();
+  setInterval(updateText, 1000);
+});
